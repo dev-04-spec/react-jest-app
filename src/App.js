583dd41b4b6adf2768f65b8729f938a6156bf00f
dev-04-spec <kalemanoj3@gmail.com>
2021-03-01 17:40:46 +0530

    React counter & Test cases functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count,setCount]=React.useState(0);
   return (
     <div data-test="component-app">
-      <h1>Hello Manoj !</h1>
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+      </h1>
+      <button data-test="increment-button" onClick={()=>setCount(count +1)}>Increment Counter</button>
     </div>
   );
 }

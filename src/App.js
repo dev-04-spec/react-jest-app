2987ea0771fbc1ea8667b8edd7f6d9a1d4624d7d
dev-04-spec <kalemanoj3@gmail.com>
2021-03-01 14:34:50 +0530

    Started Testing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
+    <div data-test="component-app">
+      <h1>Hello Manoj !</h1>
     </div>
   );
 }
